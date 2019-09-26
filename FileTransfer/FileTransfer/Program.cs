a66b9f98c392de6219b1977d624a2e761809eb41
Jari Mutanen <jari.mutanen@inteno.fi>
2019-09-26 17:55:38 +0300

    Initial commit

---
+++ FileTransfer/FileTransfer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace FileTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\jari\AppData\Roaming\Dropbox\testi.txt";
+            if (File.Exists(path))
+            {
+                //open the file for reading
+                StreamReader sr = File.OpenText(path);
+            }
+
+            }
+        }
+}
